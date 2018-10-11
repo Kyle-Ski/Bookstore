4e3ac056148b52703cb09cb8ceec8bbc8812c3fe
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-11 12:08:03 -0600

    passing isFilterByAuthor down to BookTitles.js

--- src/BookTitles.js
+++ src/BookTitles.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const BookTitles = ({bookTitleAPI, addToCart}) => {
+const BookTitles = ({bookTitleAPI, addToCart, isFilterByAuthor}) => {
     const addTitle = (json) => {
+
         return json.map((book,i) => {
             return (
                 <button key={i} id="booksList" type="button" onClick={addToCart} value={book.title} className="list-group-item list-group-item-action">
-                {/* <div key={i}   value={book.title} className="container-fluid"> */}
-                {/* <div key={i} value={book.title} className="list-group-item col-md-12"> */}
                 {book.title}: {book.subtitle}<br/>
                 by: {book.author}<br/>
                 Length: {book.pages} pages, Price: ${book.price}
-                {/* </div>
-                </div> */}
                 </button>
             )
         })
